b29902d8e5a6ef50d1d10027e035b08fb60ea024
박상용 <sangyong.park@monolith.co.kr>
2022-03-23 19:58:27 +0900

    feat : 실행될 자바 메인 진입점 만들기

---
+++ src/test/java/com/fastcampus/javaoop/logic/JavaSortTest.java
@@ -0,0 +1,25 @@
+package com.fastcampus.javaoop.logic;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+class JavaSortTest {
+
+    @Test
+    @DisplayName("자바소트 - 리스트를 넣으면 정렬된 결과를 보여준다.")
+    void given_List_WhenExecuting_ThenReturnSortedList() {
+
+
+        JavaSort<Integer> bubbleSort = new JavaSort<>();
+
+        List<Integer> actual = bubbleSort.sort(List.of(3, 2, 5, 6, 1));
+
+        assertEquals(List.of(1,2,3,5,6), actual);
+
+    }
+}
